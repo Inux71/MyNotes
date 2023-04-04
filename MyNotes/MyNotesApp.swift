8da66a3d89777c5b0b064d4f4e52059e9b96cd79
Kacper Grabiec <grabiec.kacper01@gmail.com>
2023-04-04 13:14:46 +0200

    Initial Commit

---
+++ MyNotes/MyNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNotesApp.swift
+//  MyNotes
+//
+//  Created by Kacper Grabiec on 04/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
