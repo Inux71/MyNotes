39f6fad39eced0875485f02cf45cae59b4abb447
Kacper Grabiec <grabiec.kacper01@gmail.com>
2023-04-18 14:14:21 +0200

    add Navigation 🗺️

--- MyNotes/MyNotesApp.swift
+++ MyNotes/MyNotesApp.swift
@@ -9,9 +9,32 @@ import SwiftUI
 
 @main
 struct MyNotesApp: App {
+    @State private var _path: [String] = []
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack(path: self.$_path) {
+                NoteListView(
+                    onNavigateToAddNoteView: {
+                        self._path.append("add")
+                    }
+                )
+                    .navigationDestination(for: String.self) { destination in
+                        self.getView(for: destination)
+                    }
+            }
         }
+    }
+    
+    private func getView(for destination: String) -> AnyView {
+        switch destination {
+        case "add":
+            return AnyView(AddNoteView())
+        case "edit":
+            return AnyView(NoteEditView())
+        default:
+            return AnyView(EmptyView())
+        }
+        
     }
 }
--- MyNotes/view/NoteListView.swift
+++ MyNotes/view/NoteListView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct NoteListView: View {
+    var onNavigateToAddNoteView: () -> Void
+    
     var body: some View {
         VStack {
             
