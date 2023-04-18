b472bb5979b247352eeb2fb0ae8a16b6ce850af8
Kacper Grabiec <grabiec.kacper01@gmail.com>
2023-04-18 13:39:20 +0200

    add views files 🏞️

---
+++ MyNotes/view/AddNoteView.swift
@@ -0,0 +1,16 @@
+//
+//  AddNoteView.swift
+//  MyNotes
+//
+//  Created by Kacper Grabiec on 18/04/2023.
+//
+
+import SwiftUI
+
+struct AddNoteView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
---
+++ MyNotes/view/NoteListView.swift
@@ -0,0 +1,16 @@
+//
+//  NoteListView.swift
+//  MyNotes
+//
+//  Created by Kacper Grabiec on 18/04/2023.
+//
+
+import SwiftUI
+
+struct NoteListView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
