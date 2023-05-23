d4e2194ef78e0ebc142971ef501069295df60c5e
Łukasz Myśliwiec <lmysliwiec10@gmail.com>
2023-05-23 12:45:05 +0200

    fix: fix notes list view after merge

--- MyNotes/view/NoteListView.swift
+++ MyNotes/view/NoteListView.swift
@@ -8,29 +8,31 @@
 import SwiftUI
 
 struct NoteListView: View {
-    @EnvironmentObject var notesStore: NotesStore
-    @State private var _search: String = "";
-    
-    var body: some View {
-        VStack {
-            List(notesStore.notes) { note in
-                NavigationLink(destination: NotePreviewView(note: note)) {
-                    HStack {
-                        Text(note.title)
-                    }
-                }
-                .onDelete { idx in
-                    notesStore.remove(offset: idx)
-                }
+  @EnvironmentObject var notesStore: NotesStore
+  @State private var _search: String = ""
+
+  var body: some View {
+    VStack {
+      List {
+        ForEach(notesStore.notes) { note in
+          NavigationLink(destination: NotePreviewView(note: note)) {
+            HStack {
+              Text(note.title)
             }
+          }
         }
-        .navigationTitle("Notatki")
-        .navigationBarTitleDisplayMode(.large)
-        .searchable(text: $_search)
-        .toolbar {
-            NavigationLink(value: "add") {
-                Image(systemName: "doc.badge.plus")
-            }
+        .onDelete { idx in
+          notesStore.remove(offset: idx)
         }
+      }
     }
+    .navigationTitle("Notatki")
+    .navigationBarTitleDisplayMode(.large)
+    .searchable(text: $_search)
+    .toolbar {
+      NavigationLink(value: "add") {
+        Image(systemName: "doc.badge.plus")
+      }
+    }
+  }
 }
