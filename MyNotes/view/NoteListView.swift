d33cee9dc161c7c583c97176142c45855c88b8e6
Piotr Kołodziejski <piotr.kolodziejski@sap.com>
2023-06-11 21:14:36 +0200

    Add delete all notes after shake

--- MyNotes/view/NoteListView.swift
+++ MyNotes/view/NoteListView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct NoteListView: View {
     @EnvironmentObject var notesStore: NotesStore
+    @State private var isPresentingConfirm: Bool = false
 
     var body: some View {
         VStack {
@@ -23,6 +24,14 @@ struct NoteListView: View {
                 .onDelete { idx in
                     self.notesStore.remove(offset: idx)
                 }
+                .onReceive(NotificationCenter.default.publisher(for: .deviceDidShakeNotification)) {
+                    _ in
+                    isPresentingConfirm = true
+                }
+            }.confirmationDialog("Delete all notes?",isPresented: $isPresentingConfirm) {
+                Button("Delete all notes", role: .destructive) {
+                    self.notesStore.notes.removeAll()
+                }
             }
         }
         .navigationTitle("Notatki")
@@ -32,5 +41,18 @@ struct NoteListView: View {
                 Image(systemName: "doc.badge.plus")
             }
         }
+    }
+}
+
+
+extension NSNotification.Name {
+    public static let deviceDidShakeNotification = NSNotification.Name("MyDeviceDidShakeNotification")
+}
+
+
+extension UIWindow {
+    open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        super.motionEnded(motion, with: event)
+        NotificationCenter.default.post(name: .deviceDidShakeNotification, object: event)
     }
 }
